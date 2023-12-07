42471efb5c40d1f43c20a3892c0dadaf92de09fd
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-12-07 16:41:17 +0900

    AuthContextの利用

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useContext, useState }from "react";
 import { signInWithGoogle } from "../service/firebase";
+import { AuthContext } from "../providers/AuthProvider";
 
 const Header = () => {
+  const { currentUser } = useContext(AuthContext);
+  console.log("header");
+  console.log(currentUser);
   return(
     <header>
       ヘッダー
--- src/service/firebase.js
+++ src/service/firebase.js
@@ -14,6 +14,7 @@ firebase.initializeApp({
 });
 
 const googleProvider = new firebase.auth.GoogleAuthProvider();
+export const auth = firebase.auth();
 
 export const signInWithGoogle = () => {
   firebase
